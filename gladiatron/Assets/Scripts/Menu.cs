a828a7af1f5123a8c355d05ab073c6cfc690fe94
lobo <matheus.v.lobo@gmail.com>
2020-03-25 20:20:56 -0300

    atualizacao
    atualização de arquivos

---
+++ gladiatron/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void CarregarFase1()
+    {
+        SceneManager.LoadScene("FASE1");
+    }
+    public void CarregarFase2()
+    {
+        SceneManager.LoadScene("FASE2");
+    }
+    public void VoltaMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void SairAPK()
+    {
+        Application.Quit();
+    }
+}
---
+++ gladiatron/Assets/Scripts/Player_mov.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+
+public class Player_mov : MonoBehaviour
+{
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+    protected bool jump;
+    public int velocidade = 10;
+    //private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+        //anm = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(joystick.Horizontal * velocidade + Input.GetAxis("Horizontal") * velocidade,
+            rigidbody.velocity.y, joystick.Vertical * velocidade + Input.GetAxis("Vertical") * velocidade);
+
+        if (!jump && (joybutton.Pressed || Input.GetButton("Fire2")))
+        {
+            jump = true;
+            rigidbody.velocity += Vector3.up * 3f;
+            
+        }
+        if(jump && (!joybutton.Pressed || Input.GetButton("Fire2")))
+        {
+            jump = false;
+        }
+
+    }
+}
