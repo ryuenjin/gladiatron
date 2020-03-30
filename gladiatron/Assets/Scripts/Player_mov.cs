225bbb9cc8023eb1afb8eab9a77c808d3542a4e2
lobo <matheus.v.lobo@gmail.com>
2020-03-30 16:18:14 -0300

    atualização de segurança
    Atualização de componentes, apagando a pasta app, e tentando animar o player

--- gladiatron/Assets/Scripts/Player_mov.cs
+++ gladiatron/Assets/Scripts/Player_mov.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 
@@ -10,33 +11,50 @@ public class Player_mov : MonoBehaviour
 {
     protected Joystick joystick;
     protected Joybutton joybutton;
-    protected bool jump;
-    public int velocidade = 10;
-    //private Animator anim;
+    public float velocidade = 10; //velocidade de movimento
+    public float VelMax = 200;
+    Animator anim;//chama as animações
+    protected bool Jump;
+    private float JumpTime;//limitando o pulo do Tigas
     // Start is called before the first frame update
     void Start()
     {
         joystick = FindObjectOfType<Joystick>();
         joybutton = FindObjectOfType<Joybutton>();
-        //anm = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
 
 
     void Update()
     {
+
         var rigidbody = GetComponent<Rigidbody>();
+        
         rigidbody.velocity = new Vector3(joystick.Horizontal * velocidade + Input.GetAxis("Horizontal") * velocidade,
             rigidbody.velocity.y, joystick.Vertical * velocidade + Input.GetAxis("Vertical") * velocidade);
 
-        if (!jump && (joybutton.Pressed || Input.GetButton("Fire2")))
+        //setando as animaçãoes do player
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            jump = true;
+            anim.SetBool("Correndo", true);
+        }
+        else
+        {
+            anim.SetBool("Correndo", false);
+            
+        }
+
+        
+
+        if (!Jump && (joybutton.Pressed || Input.GetButton("Fire2")))
+        {
+            Jump = true;
             rigidbody.velocity += Vector3.up * 3f;
             
         }
-        if(jump && (!joybutton.Pressed || Input.GetButton("Fire2")))
+        if(Jump && (!joybutton.Pressed || Input.GetButton("Fire2")))
         {
-            jump = false;
+            Jump = false;
         }
 
     }
