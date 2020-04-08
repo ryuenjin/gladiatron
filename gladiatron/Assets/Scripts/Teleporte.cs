06ec8b6908707ac311c57d4d1c736c0875172ed6
lobo <matheus.v.lobo@gmail.com>
2020-04-08 15:40:14 -0300

    atualizacao de mapa

---
+++ gladiatron/Assets/Scripts/Teleporte.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(AudioSource))]
+public class Teleporte : MonoBehaviour
+{
+
+    public Transform[] destinos;
+    public AudioClip audioTeleporte;
+    [Space(10)]
+    public bool destruirAoColidir = false;
+    AudioSource emissorDeSom;
+    bool rotinaIniciada = false;
+
+    void Awake()
+    {
+        emissorDeSom = GetComponent<AudioSource>();
+        emissorDeSom.playOnAwake = false;
+        emissorDeSom.loop = false;
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (destinos.Length > 0 && !rotinaIniciada)
+        {
+            int destino = Random.Range(0, destinos.Length);
+            if (destinos[destino])
+            {
+                other.transform.position = destinos[destino].position;
+                other.transform.rotation = destinos[destino].rotation;
+                if (destruirAoColidir)
+                {
+                    StartCoroutine("EsperarParaDestruir");
+                }
+            }
+            if (audioTeleporte)
+            {
+                emissorDeSom.clip = audioTeleporte;
+                emissorDeSom.PlayOneShot(emissorDeSom.clip);
+            }
+        }
+    }
+
+    IEnumerator EsperarParaDestruir()
+    {
+        rotinaIniciada = true;
+        MeshRenderer mesh = GetComponent<MeshRenderer>();
+        if (mesh)
+        {
+            mesh.enabled = false;
+        }
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+}
