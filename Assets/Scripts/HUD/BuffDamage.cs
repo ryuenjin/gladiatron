966d9e77af3a60aedc80e23a363c4439a89abb56
lobo <matheus.v.lobo@gmail.com>
2020-05-14 20:13:02 -0300

    atualizando

---
+++ Assets/Scripts/HUD/BuffDamage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffDamage : MonoBehaviour
+{
+    public LayerMask playerLayers;
+
+    int dano = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)// função para dar dano
+    {
+
+        other.gameObject.GetComponent<Player_Attack>().Dano(dano);
+        DestroyGameObject();
+
+    }
+
+    public virtual void DestroyGameObject()
+    {
+        Destroy(gameObject, 0.5f);
+    }
+
+}
